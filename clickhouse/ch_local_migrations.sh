4d80787775b29d9e694dfce29114dd91f03e9a27
David Pacheco <David.Pacheco@fcm.digital>
2024-02-29 09:36:14 +0100

    Update ch_local_migrations.sh

--- clickhouse/ch_local_migrations.sh
+++ clickhouse/ch_local_migrations.sh
@@ -12,7 +12,7 @@ DEFAULT_CLICKHOUSE_HOST="localhost"
 DEFAULT_CLICKHOUSE_PORT="9000"
 DEFAULT_CLICKHOUSE_USER="default"
 DEFAULT_CLICKHOUSE_PASSWORD="" # Leave blank if no password
-DEFAULT_CLICKHOUSE_QUERY_SETTINGS="" # Leave blank if want to use default settings
+DEFAULT_CLICKHOUSE_DISK_SETTINGS="" # Leave blank if want to use default disk (local storage)
 
 # Override with command line arguments if provided
 CLICKHOUSE_DB="${1:-$DEFAULT_CLICKHOUSE_DB}"
@@ -20,7 +20,7 @@ CLICKHOUSE_HOST="${2:-$DEFAULT_CLICKHOUSE_HOST}"
 CLICKHOUSE_PORT="${3:-$DEFAULT_CLICKHOUSE_PORT}"
 CLICKHOUSE_USER="${4:-$DEFAULT_CLICKHOUSE_USER}"
 CLICKHOUSE_PASSWORD="${5:-$DEFAULT_CLICKHOUSE_PASSWORD}"
-CLICKHOUSE_QUERY_SETTINGS="${6:-$DEFAULT_CLICKHOUSE_QUERY_SETTINGS}"
+CLICKHOUSE_DISK_SETTINGS="${6:-$DEFAULT_CLICKHOUSE_DISK_SETTINGS}"
 
 
 # Utility function to create connection string
@@ -35,7 +35,7 @@ create_migration_table() {
     local ch_conn_str=$(create_clickhouse_string_connection)
 
     local query="CREATE TABLE IF NOT EXISTS helicone_migrations (migration_name String, applied_date DateTime DEFAULT now()) ENGINE = MergeTree() ORDER BY migration_name"
-    [[ -n "$CLICKHOUSE_QUERY_SETTINGS" ]] && query+=" SETTINGS $CLICKHOUSE_QUERY_SETTINGS"
+    [[ -n "$CLICKHOUSE_DISK_SETTINGS" ]] && query+=" SETTINGS $CLICKHOUSE_DISK_SETTINGS"
     query+=";"
 
     clickhouse-client $ch_conn_str --query "$query"
@@ -96,9 +96,11 @@ run_migrations() {
             echo "Applying migration from file: $file"
 
             local query="$(cat $MIGRATIONS_DIR/$file)"
-            query="${query%;}"
-            [[ -n "$CLICKHOUSE_QUERY_SETTINGS" ]] && query+=" SETTINGS $CLICKHOUSE_QUERY_SETTINGS"
-            query+=";"
+            if [[ "$query" == *"CREATE TABLE"* ]]; then # Disk configuration only will be applied in 'CREATE TABLE' queries
+                query="${query%;}"
+                [[ -n "$CLICKHOUSE_DISK_SETTINGS" ]] && query+=" SETTINGS $CLICKHOUSE_DISK_SETTINGS"
+                query+=";"
+            fi
 
             clickhouse-client $ch_conn_str --query "$(echo "$query" | tr '\n' ' ')"
             if [ $? -eq 0 ]; then
